c16ddf4e04ed10a6ed018e54b08c8479abfe1fe7
Daniele <daniele.cesini@gmail.com>
2021-04-23 15:05:51 +0200

    Update attaching_a_volume.sh

--- attaching_a_volume.sh
+++ attaching_a_volume.sh
@@ -9,6 +9,22 @@ mkdir /data2
 yum install vim
 vim /etc/fstab
 # add the line: /dev/xvdf1     /data2  ext4 defaults 0 0
+
+###############################
+#Inspect the fstaba file
+[root@ip-172-31-12-71 ~]# cat /etc/fstab
+
+#
+# /etc/fstab
+# Created by anaconda on Mon Jan 28 15:24:25 2019
+#
+# Accessible filesystems, by reference, are maintained under '/dev/disk'
+# See man pages fstab(5), findfs(8), mount(8) and/or blkid(8) for more info
+#
+UUID=4a1c93d9-eb47-4f96-9f3d-920e52dc8cca /                       xfs     defaults        0 0
+/dev/xvdf1     /data2  ext4 defaults 0 0
+##########################################################
+
 mount -a
 df -h
 ll /data2
